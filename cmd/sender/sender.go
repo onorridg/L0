b7d67203955f84f8ddf29da06ed054c1743ec9ae
onorridg <yatruespy@gmail.com>
2023-06-15 19:14:14 +0300

    add messages processing logic for multiple goroutines

--- cmd/sender/sender.go
+++ cmd/sender/sender.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"github.com/nats-io/stan.go"
 	"l0/internal/env"
+	"log"
 )
 
 func main() {
 	// Подключение к Nats Streaming Server
-	//cluster := env.Get().NatsClusterId
-	fmt.Println(env.Get().PgPort, env.Get().NatsPort, env.Get().NatsPortHttp)
+	cluster := env.Get().NatsClusterId
 
-	//sc, err := stan.Connect(cluster, "onorridg")
-	//if err != nil {
-	//	log.Fatalf("Failed to connect to Nats Streaming: %v", err)
-	//}
-	//defer sc.Close()
-	//
-	//// Отправка сообщения в канал
-	//err = sc.Publish("order", []byte("Test group"))
-	//if err != nil {
-	//	log.Fatalf("Failed to publish message: %v", err)
-	//}
-	//
-	//log.Println("Message published successfully.")
+	sc, err := stan.Connect(cluster, "onorridg")
+	if err != nil {
+		log.Fatalf("Failed to connect to Nats Streaming: %v", err)
+	}
+	defer sc.Close()
+
+	// Отправка сообщения в канал
+	err = sc.Publish("order", []byte("Test group"))
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+	}
+
+	log.Println("Message published successfully.")
 }
--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,47 +1,87 @@
 package worker
 
 import (
-	"log"
-
+	"context"
+	"fmt"
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/stan.go"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 )
 
 func msgHandler(msg *stan.Msg) {
+	log.Printf("Получено сообщение: %s\n", string(msg.Data))
+}
+
+type workerData struct {
+	ctx context.Context
+	sc  stan.Conn
+	sub stan.Subscription
+	id  uint8
+}
+
+func worker(wD *workerData) {
+	defer func() {
+		err := wD.sub.Unsubscribe()
+		if err != nil {
+			log.Println("sub:", wD.id, err)
+		}
+		err = wD.sc.Close()
+		if err != nil {
+			log.Println("sc:", wD.id, err)
+		}
+	}()
+
+main:
+	for {
+		select {
+		case <-wD.ctx.Done():
+			fmt.Println("Закругляется:", wD.id)
+			break main
+		default:
+			fmt.Println("Работает:", wD.id)
+			time.Sleep(time.Second * 3)
+		}
+	}
 
 }
 
-func initWorkers() {
-
+func runWorkers(ctx context.Context) {
+	for i := 0; i < 10; i++ {
+		wD := workerData{}
+		wD.ctx = ctx
+		wD.id = uint8(i)
+		id := "worker-" + strconv.Itoa(i)
+		var err error
+		wD.sc, err = stan.Connect("L0", id, stan.NatsURL(nats.DefaultURL))
+		if err != nil {
+			log.Fatal(err)
+		}
+		//defer sc.Close()
+		wD.sub, err = wD.sc.QueueSubscribe("order", "order-workers", msgHandler,
+			stan.DurableName("order-workers"),
+			stan.MaxInflight(10)) // подписчик может обрабатывать не более 10 сообщений одновременно
+		if err != nil {
+			log.Fatal(err)
+		}
+		go worker(&wD)
+		//defer sub.Unsubscribe()
+	}
 }
 
 func Run() {
-	// Создание подключения к серверу NATS Streaming
-	sc, err := stan.Connect("L0", "worker-2", stan.NatsURL(nats.DefaultURL))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sc.Close()
+	ctx, cancel := context.WithCancel(context.Background())
+	runWorkers(ctx)
 
-	// Определение функции обработчика сообщений
-	handler := func(msg *stan.Msg) {
-		log.Printf("Получено сообщение: %s\n", string(msg.Data))
-		// Обработка сообщения
-	}
-
-	// Подписка на канал с использованием опции DurableName и названия группы подписчиков
-	sub, err := sc.QueueSubscribe(
-		"order",
-		"order-workers",
-		handler,
-		stan.DurableName("order-workers"),
-		stan.MaxInflight(10)) // подписчик может обрабатывать не более 10 сообщений одновременно
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sub.Unsubscribe()
-
-	// Ожидание завершения программы
-	select {}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	cancel()
+	log.Println("Shutdown workers ...")
+	time.Sleep(time.Second * 8)
+	log.Println("Workers exiting")
 }
